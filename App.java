b24521d24a6cd9d66f413c314e27c7e68eb7fdf2
Josué Luís Pimenta Alecrim <JosueAlecrim7@gmail.com>
2026-04-28 20:11:56 -0300

    Update App.java

--- App.java
+++ App.java
@@ -7,9 +7,9 @@ public class App {
         System.out.println(+ array[i]);
         }
     }
-public static void main(String[]args) {
-    int[] numeros = {10, 20, 30, 40, 50};
-    imprimirArray(numeros);
-}
+       public static void main(String[]args) {
+     int[] numeros = {10, 20, 30, 40, 50};
+   imprimirArray(numeros);
+  }
 }
 
